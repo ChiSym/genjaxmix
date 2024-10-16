fix: correct formula for betas in gem's logpdf

src/genspn/distributions.py
@@ -191,8 +191,11 @@ def logpdf(dist: Mixed, x: Datapoint) -> Float[Array, ""]:
 
 @dispatch
 def logpdf(dist: GEM, pi: Float[Array, "n"], K: Integer[Array, ""]) -> Float[Array, ""]:
-    betas = jax.vmap(lambda i: 1 - pi[i] / pi[i-1])(jnp.arange(len(pi)))
-    betas = betas.at[0].set(pi[0])
+    def unfold(carry, pi):
+        beta = pi / carry
+        return carry * (1-beta), beta
+
+    _, betas = jax.lax.scan(unfold, 1.0, pi)
     logprobs = jax.vmap(jax.scipy.stats.beta.logpdf, in_axes=(0, None, 0))(betas, 1-dist.d, dist.alpha + (1 + jnp.arange(len(pi))) * dist.d)
     idx = jnp.arange(logprobs.shape[0])
     logprobs = jnp.where(idx < K, logprobs, 0) 

test/test_score.py
@@ -24,15 +24,12 @@ def test_score_stick_breaking():
     logp0 = logpdf(gem, pis0, K)
     logp1 = logpdf(gem, pis1, K)
 
-    assert logp0 == (
-        jax.scipy.stats.beta.logpdf(pis0[0], 1 - d, alpha + d) +
-        jax.scipy.stats.beta.logpdf(1 - pis0[1]/pis0[0], 1 - d, alpha + 2 * d)
-    )
+    logp0_expected = jax.scipy.stats.beta.logpdf(pis0[0], 1 - d, alpha + d) + jax.scipy.stats.beta.logpdf(pis0[1]/(1-pis0[0]), 1 - d, alpha + 2 * d)
+    assert logp0 == logp0_expected 
 
-    assert logp1 == (
-        jax.scipy.stats.beta.logpdf(pis1[0], 1 - d, alpha + d) +
-        jax.scipy.stats.beta.logpdf(1 - pis1[1]/pis1[0], 1 - d, alpha + 2 * d)
-    )
+    logp1_expected = jax.scipy.stats.beta.logpdf(pis1[0], 1 - d, alpha + d) + jax.scipy.stats.beta.logpdf(pis1[1]/(1-pis1[0]), 1 - d, alpha + 2 * d)
+
+    assert logp1 == logp1_expected
 
 def test_score_data():
     x = jnp.zeros((5, 6))