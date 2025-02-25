7bbdc6fae7a907451eb558dc722d5f572bbeec69
Ian Limarta <ian.limarta@gmail.com>
2025-02-25 01:48:18 -0600

    more changes

--- test/test_parameter_proposal.py
+++ test/test_parameter_proposal.py
@@ -1,12 +1,12 @@
 import jax
 import jax.numpy as jnp
 import genjaxmix.model.dsl as dsl
-import genjaxmix.model.compile as compile
+import genjaxmix.model as mm
 
 
 def test_normal_normal_known_mu():
     # TODO: Swap this out with a Markov blanket and call build_parameter_proposal directly
-    class Test(compile.Model):
+    class Test(mm.Model):
         def __init__(self):
             super().__init__()
             self.mu = dsl.Normal(jnp.zeros((1, 1)), jnp.ones((1, 1)))
@@ -39,8 +39,8 @@ def test_normal_normal_known_mu():
     empirical_mean = jnp.mean(output)
     empirical_std = jnp.std(output)
 
-    expected_mean = 0.0
-    expected_std = 0.0
+    expected_mean = jnp.array(0.27979577)
+    expected_std = jnp.array(0.74080914)
 
     assert jnp.isclose(expected_mean, empirical_mean)
     assert jnp.isclose(expected_std, empirical_std)
